ba5d45a8460d58716cceaf6abe6bb60ddb0b0ce1
rmedard <00Derfine@5>
2017-07-19 21:50:49 +0200

    Kubuntu

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,5 @@
 jQuery('select#edit-field-advert-district-target-id > option').each(function () {
-    if ((this).attr("value").inArray(75,76,77,78,79)){
-        (this).attr("disabled", true);
+    if (this.attr("value").inArray(75,76,77,78,79)){
+        this.attr("disabled", true);
     }
 });
