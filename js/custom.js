00211dfc94df3a4b87d4a66b379c22a7e5449be8
rmedard <00Derfine@5>
2017-07-19 22:00:11 +0200

    Kubuntu

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,12 @@
-jQuery('select#edit-field-advert-district-target-id > option').each(function ($) {
-    if ($(this).attr("value").inArray(75,76,77,78,79)){
-        $(this).attr("disabled", true);
+// jQuery('select#edit-field-advert-district-target-id > option').each(function ($) {
+//     if ($(this).attr("value").inArray(75,76,77,78,79)){
+//         $(this).attr("disabled", true);
+//     }
+// });
+
+(function ($) {
+    var provinceOption = $('select#edit-field-advert-district-target-id > option');
+    if (provinceOption.attr('value').inArray(75,76,77,78,79)){
+        provinceOption.attr('disabled', true);
     }
-});
+}(jQuery));
