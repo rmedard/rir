2709aaab5a3b7a872fa18c226f0c2ad795326a24
rmedard <00Derfine@5>
2017-07-19 22:16:45 +0200

    Kubuntu

--- js/custom.js
+++ js/custom.js
@@ -5,7 +5,8 @@
 // });
 
 (function ($) {
-    if ($.inArray($('select#edit-field-advert-district-target-id > option').attr('value'), ['75','76','77','78','79'])){
+    if ($.inArray($('select#edit-field-advert-district-target-id > option').attr('value'),
+            [75,76,77,78,79])){
         $(this).attr('disabled', true)
     }
 }(jQuery));
