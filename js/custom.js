edda844c3cdef276749c9b7ed9afcb532c098d6d
rmedard <00Derfine@5>
2017-07-19 22:06:05 +0200

    Kubuntu

--- js/custom.js
+++ js/custom.js
@@ -5,8 +5,9 @@
 // });
 
 (function ($) {
-    var provinceOption = $('select#edit-field-advert-district-target-id > option');
-    if (provinceOption.attr('value').inArray(75,76,77,78,79)){
-        provinceOption.attr('disabled', true);
-    }
+    $('select#edit-field-advert-district-target-id > option').each(function () {
+        if ($(this).attr('value') == 75){
+            $(this).attr('disabled', true)
+        }
+    });
 }(jQuery));
