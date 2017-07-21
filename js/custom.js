7fd0e4eb165e4caf2a00b8acb8e788302c640b59
rmedard <00Derfine@5>
2017-07-21 19:51:36 +0200

    Kubuntu

--- js/custom.js
+++ js/custom.js
@@ -1,12 +1,10 @@
-// jQuery('select#edit-field-advert-district-target-id > option').each(function ($) {
-//     if ($(this).attr("value").inArray(75,76,77,78,79)){
-//         $(this).attr("disabled", true);
-//     }
-// });
-
 (function ($) {
     $('select#edit-field-advert-district-target-id > option').each(function () {
-        if ($.inArray($(this).attr('value'), ['75','76','77','78','79'])){
+        if ($(this).attr('value') == 75 ||
+            $(this).attr('value') == 76 ||
+            $(this).attr('value') == 77 ||
+            $(this).attr('value') == 78 ||
+            $(this).attr('value') == 79){
             $(this).attr('disabled', true)
         }
     });
