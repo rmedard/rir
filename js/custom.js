c0cfb63c10e055feaa83e1b16dae6c0a14f19f9d
rmedard <00Derfine@5>
2017-07-24 00:25:54 +0200

    Kubuntu

--- js/custom.js
+++ js/custom.js
@@ -13,15 +13,18 @@
     $('button#edit-submit-adverts').removeClass('btn-xs').addClass('btn-sm');
     $('div.view-adverts > div.view-filters > form.views-exposed-form > div.form-inline > div.form-item > div.select-wrapper > select').addClass('input-sm');
 
+    //Activate primary menu while navigating secondary menu
     if ($(location).attr('href').match('((\\/)(adverts\\/rent\\/(([a-z])\\w+))?$)')){
         $('nav#block-advertsprimarymenu > ul.nav > li:first-child').addClass('active');
     } else if ($(location).attr('href').match('((\\/)(adverts\\/buy\\/(([a-z])\\w+))?$)')){
         $('nav#block-advertsprimarymenu > ul.nav > li:last-child').addClass('active');
     }
+    //End
 
+    //Activate home link while on homepage
     if ($('nav#block-rir-main-menu > ul.nav > li:first-child > a').hasClass('is-active')){
-        console.log("should be okay");
         $('nav#block-rir-main-menu > ul.nav > li:first-child').addClass('active');
     }
+    //End
 
 }(jQuery));
