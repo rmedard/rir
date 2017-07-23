4fbab34870dac2c39091146ab2320fb6364186a5
rmedard <00Derfine@5>
2017-07-23 17:58:34 +0200

    Kubuntu

--- js/custom.js
+++ js/custom.js
@@ -11,13 +11,12 @@
     $('div.view-adverts > div.view-filters').addClass('well well-sm');
     $('button#edit-submit-adverts').removeClass('btn-xs');
     $('button#edit-submit-adverts').addClass('btn-sm');
-    $('form#views-exposed-form-adverts-page-adverts-all > div.form-inline > div.form-item > div.select-wrapper > select').addClass('input-sm');
+    $('div.view-adverts > div.view-filters > form.views-exposed-form > div.form-inline > div.form-item > div.select-wrapper > select').addClass('input-sm');
 
     if ($(location).attr('href').match('((\\/)(adverts\\/rent\\/(([a-z])\\w+))?$)')){
         $('nav#block-advertsprimarymenu > ul.nav > li:first-child').addClass('active');
     } else if ($(location).attr('href').match('((\\/)(adverts\\/buy\\/(([a-z])\\w+))?$)')){
         $('nav#block-advertsprimarymenu > ul.nav > li:last-child').addClass('active');
     }
-
 
 }(jQuery));
