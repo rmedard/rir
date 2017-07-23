76908cbcfd4e5c7baec8bea3ac9b03d061f537e5
rmedard <00Derfine@5>
2017-07-23 19:41:57 +0200

    Kubuntu

--- js/custom.js
+++ js/custom.js
@@ -1,17 +1,16 @@
 (function ($) {
     $('select#edit-field-advert-district-target-id > option').each(function () {
-        if ($(this).attr('value') === 75 ||
-            $(this).attr('value') === 76 ||
-            $(this).attr('value') === 77 ||
-            $(this).attr('value') === 78 ||
-            $(this).attr('value') === 79){
+        if ($(this).attr('value') === '75' ||
+            $(this).attr('value') === '76' ||
+            $(this).attr('value') === '77' ||
+            $(this).attr('value') === '78' ||
+            $(this).attr('value') === '79'){
             $(this).attr('disabled', true);
         }
     });
 
     $('div.view-adverts > div.view-filters').addClass('well well-sm');
-    $('button#edit-submit-adverts').removeClass('btn-xs');
-    $('button#edit-submit-adverts').addClass('btn-sm');
+    $('button#edit-submit-adverts').removeClass('btn-xs').addClass('btn-sm');
     $('div.view-adverts > div.view-filters > form.views-exposed-form > div.form-inline > div.form-item > div.select-wrapper > select').addClass('input-sm');
 
     if ($(location).attr('href').match('((\\/)(adverts\\/rent\\/(([a-z])\\w+))?$)')){
