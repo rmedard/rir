f2696cf4321dd3cf4e5735e76145e68810c5f365
Medard <rmedard5@gmail.com>
2017-07-24 12:30:58 +0200

    EC commit...

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,30 @@
 (function ($) {
+    var isMobile = {
+        Android: function() {
+            return navigator.userAgent.match(/Android/i);
+        },
+        BlackBerry: function() {
+            return navigator.userAgent.match(/BlackBerry/i);
+        },
+        iOS: function() {
+            return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+        },
+        Opera: function() {
+            return navigator.userAgent.match(/Opera Mini/i);
+        },
+        Windows: function() {
+            return navigator.userAgent.match(/IEMobile/i);
+        },
+        any: function() {
+            return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+        }
+    };
+
+    if (isMobile.any()){
+        $('nav#block-advertsprimarymenu > ul.nav-pills').removeClass('nav-justified').addClass('nav-mobile-primary');
+        $('nav#block-advertssecondarymenu > ul.nav-pills').removeClass('nav-justified');
+    }
+
     $('select#edit-field-advert-district-target-id > option').each(function () {
         if ($(this).attr('value') === '75' ||
             $(this).attr('value') === '76' ||
