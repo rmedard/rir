9a08ab8d3a0226b1551dd6c567d38138aa0ac203
rmedard <00Derfine@5>
2017-08-05 18:00:18 +0200

    Kubuntu

--- js/custom.js
+++ js/custom.js
@@ -66,4 +66,16 @@
 
     $('div.form-type-fivestar > div.form-type-select > div').removeClass('select-wrapper');
 
+    console.log(getParameterByName('field_advert_property_type_value'));
+
 }(jQuery));
+
+function getParameterByName(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+      results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
