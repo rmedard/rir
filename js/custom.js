c1954f1700ddcb213f9ebd2923b909b5458bf484
rmedard <00Derfine@5>
2017-08-05 19:46:15 +0200

    Kubuntu

--- js/custom.js
+++ js/custom.js
@@ -70,6 +70,7 @@
     var decoded = decodeURIComponent(uri);
     var districtsStr = decoded.substring(decoded.indexOf('field_advert_district_target_id[]'), decoded.lastIndexOf('field_advert_district_target_id[]') + 36);
     console.log(decoded);
+    console.log(decoded.match(/\d+/));
     console.log(districtsStr);
     console.log(url('field_advert_district_target_id[]', decoded));
 
