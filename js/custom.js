fdde6ec4daeffec33f94af56c1161ddd44fe8837
Medard <rmedard5@gmail.com>
2018-06-18 17:14:36 +0200

    EC

--- js/custom.js
+++ js/custom.js
@@ -8,7 +8,7 @@ function handleDeviceChange(deviceMql, $) {
                 '                    <div class="panel-heading" style="border-bottom: 1px #ccc solid">\n' +
                 '                        <a href="#search-block-element" data-toggle="collapse" class="panel-title collapsed" role="button"><span class="glyphicon glyphicon-search" aria-hidden="true"></span> Search</a>\n' +
                 '                    </div>\n' +
-                '                    <div class="panel-body panel-collapse collapse fade" id="search-block-element" style="padding:0">\n' + searchBarHtml +
+                '                    <div class="panel-body panel-collapse collapse fade" id="search-block-element" style="padding:0">' + searchBarHtml +
                 '                    </div>\n' +
                 '                </div>');
             searchBar.remove();
